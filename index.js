8446821d98fc40d02eeb77619fe8935b05eba51e
Trần Hữu Nghĩa <panyeu120@gmail.com>
2023-03-13 04:14:58 +0700

    BroadCast peer to peer

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const Blockchain = require('./Blockchain');
+const PubSub = require('./pubsub1')
 
 
 const app = express();
 const blockchain = new Blockchain();
+const pubsub= new PubSub({blockchain});
+
+
+setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
 
@@ -16,10 +21,19 @@ app.post('/api/mine',(req,res)=>{
     const {data} = req.body;
 
     blockchain.addBlock({data});
+    pubsub.broadcastChain();
 
     res.redirect('/api/blocks');
 });
 
+const DEFAULT_PORT = 3000;
+let PEER_PORT;
 
-const PORT = 3000;
-app.listen(3000,()=>console.log(`listening at localhost:${PORT}`));
+if(process.env.GENERATE_PEER_PORT==='true')
+{
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random()*1000)
+}
+
+
+const PORT = PEER_PORT||DEFAULT_PORT;
+app.listen(PORT,()=>console.log(`listening at localhost:${PORT}`));
