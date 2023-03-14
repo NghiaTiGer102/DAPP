f47293ea090dfcb24fd33c5f93903d44504ddded
Trần Hữu Nghĩa <panyeu120@gmail.com>
2023-03-14 16:26:34 +0700

    Sign Data and Verifying Signatures

--- wallet/index.js
+++ wallet/index.js
@@ -1,16 +1,20 @@
 const { STARTING_BALANCE } = require('../config');
 const {ec} = require('../util')
+const cryptoHash = require('../util/crypto-hash')
 
 
 
-class Wallet{
-    constructor(){
-        this.balance = STARTING_BALANCE;
-
-        const keyPair = ec.genKeyPair();
-
-        this.publicKey = keyPair.getPublic().encode('hex');
-
+class Wallet {
+    constructor() {
+      this.balance = STARTING_BALANCE;
+  
+      this.keyPair = ec.genKeyPair();
+  
+      this.publicKey = this.keyPair.getPublic().encode('hex');
+    }
+  
+    sign(data) {
+      return this.keyPair.sign(cryptoHash(data))
     }
 
 };
